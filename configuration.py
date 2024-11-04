a65fc83e68d823d4ac72f1ad000fe95708e84af7
NayiLuna <nayibe.ttqa@gmail.com>
2024-11-04 17:09:39 -0500

    Mi segundo commit

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE =  " https://cnt-8ad7e67c-8505-4e56-a76d-d077deb233d9.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE =  "  https://cnt-4b8d66cd-9500-4c98-8a27-1c05242941a4.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH ="/api/v1/users/"# Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits/"# Almacena la ruta para crear un kit en esta variable
